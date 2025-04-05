74869e95d7475c8175c37d65afb42c6e00ad04b0
BrennanK <bksafari17@gmail.com>
2025-04-05 16:06:07 -0500

    Examples of Optimizing STD vector
    - We looked at how to optimize our code by knowing the environment of our code and defining intentions
    - We pre-allocated space using reserve() method and passed parameters into our objects using emplace_back() method
    - This saves us from copying the objects passed into our vectors which can be slow and costly as size increases

--- ProofOfGeneralKnowledge/Entity.h
+++ ProofOfGeneralKnowledge/Entity.h
@@ -11,6 +11,18 @@ public:
 struct OffsetEx
 {
 	float a, b, c;
+
+
+
+	OffsetEx(float av,float bv,float cv) :a(av), b(bv), c(cv)
+	{
+
+	}
+
+	OffsetEx(const OffsetEx& off) : a(off.a),b(off.b),c(off.c)
+	{
+		std::cout << "Copied!" << std::endl;
+	}
 };
 
 std::ostream& operator<<(std::ostream& stream, const OffsetEx& offset);
--- ProofOfGeneralKnowledge/OptimizedVector.cpp
+++ ProofOfGeneralKnowledge/OptimizedVector.cpp
@@ -2,10 +2,24 @@
 #include <vector>
 void STDVectorNotes()
 {
-	std::vector<OffsetEx>en;
-	en.push_back({4,5,6});
-	en.push_back({ 7,8,9 });
+	/*
+	
+	2 optimizations can be done here:
+	1) Every time we add a new element we are having to dynamically changing the size and thus reqqquiring a copy
+	2) When adding an element to the vector we are having to pass an object in which makes a copy of the object to pass in
 
+	Fixes:
+	1) Since we know in this case that we will have a max size we can specify or reserve that space in memory using reserve() method
+	2) We can pass the parameters for the object instead of the whole object itself using emplace_back()
+	*/
+	std::vector<OffsetEx>en;
+	en.reserve(3);
+
+
+	en.emplace_back(4,5,6);
+	en.emplace_back(1,2,3);
+	en.emplace_back(7,8,9);
+	/*
 	for (int i = 0; i < en.size(); i++)
 	{
 		std::cout << en[i];
@@ -18,4 +32,5 @@ void STDVectorNotes()
 	{
 		std::cout << o;
 	}
+	*/
 }
