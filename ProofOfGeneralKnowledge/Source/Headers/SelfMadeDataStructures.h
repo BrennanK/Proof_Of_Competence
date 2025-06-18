c09e19ecf0f8e0e05b0ec6f5ff08371560cf4f6b
Brennan Kersey <bksafari17@gmail.com>
2025-06-18 12:53:36 -0500

    Working on Making an Array List Structure from scratch
    - Completed Constructor and Destructor
    - Need to fix copy constructor
    - Need to make move constructor and Resize function

--- ProofOfGeneralKnowledge/Source/Headers/SelfMadeDataStructures.h
+++ ProofOfGeneralKnowledge/Source/Headers/SelfMadeDataStructures.h
@@ -26,27 +26,90 @@ public:
 	
 };
 
+/*
+Goals:
+
+Ability to resize
+Constructor
+Destructor
+Copy
+Move
+*/
+
+template<typename dt,size_t s>
+class ArrayList
+{
+private:
+	dt* m_data;
+	size_t m_size;
+public: 
+	ArrayList() :m_size(s) { m_data = new dt[s]; };
+	~ArrayList() { delete[] m_data; }; // Need to fix this so that no problem occurs in having the same memory freed twice
+	ArrayList(const ArrayList& data):m_size(data.m_size)
+	{
+		m_data = new dt[m_size];
+		memcpy(m_data, data.m_data, m_size * sizeof(dt));
+		//delete[] data.m_data;
+	}
+
+public:
+	size_t size() { return m_size; }
+	dt& operator[](size_t index){ return m_data[index];}
+
+	void AddIndex(dt Item)
+	{
+		// To DO
+
+	}
+	void PrintList()
+	{
+		for (size_t i = 0; i < m_size; i++)
+		{
+			std::cout << "List Item[" << i << "]: " << m_data[i] << "\n";
+		}
+	}
+};
+
 void SelfMadeStructureExample()
 {
+#pragma region Static_Array_Example
 	SM_Array<int, 3> myInts;
 
 	static_assert(myInts.Size() < 10, "The data size is too big"); // One method of evaluating at runtime
 
-	LOG(myInts.Size());
+	//LOG(myInts.Size());
 
 	myInts[0] = 0;
 
-	LOG(myInts[0]);
+	//LOG(myInts[0]);
 
 	const auto& customeArrayRef = myInts;
 
 	myInts[1] = 12;
 
-	LOG(customeArrayRef[1]);
+	//LOG(customeArrayRef[1]);
 
 	memset(&myInts[0], 0, myInts.Size() * sizeof(int));
 	for (size_t i = 0; i < myInts.Size(); i++)
 	{
-		LOG(myInts[i]);
+		//LOG(myInts[i]);
 	}
+#pragma endregion
+
+#pragma region Array_List_Example
+	ArrayList<std::string, 2>Names;
+	std::cout << "The ArrayList Size is: "<<Names.size()<<"\n";
+	Names[0] = "Safari";
+	Names[1] = "rules";
+	std::cout << "Names index 0: " << Names[0] << "\n";
+
+	ArrayList<std::string, 2> Names2 = Names;
+
+	LOG("Names");
+	Names.PrintList();
+
+	LOG("Names2");
+	Names.PrintList();
+#pragma endregion 
 }
+
